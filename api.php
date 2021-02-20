32dbfb63d89003d75992b401e7a5eabc5841a9ee
Nimityx <c93c81b9-b581-4651-89fc-a416fd69558e@anonaddy.me>
2021-02-20 15:35:51 +0700

    Delete api.php

--- api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ($_GET['type'] == "host-json") {
-    echo file_get_contents("https://raw.githubusercontent.com/Nimityx/lists/main/host.json");
-} elseif ($_GET['type'] == "accessible-host-json") {
-    echo file_get_contents("https://raw.githubusercontent.com/Nimityx/lists/main/accessible-host.json");
-}
-
-?>
